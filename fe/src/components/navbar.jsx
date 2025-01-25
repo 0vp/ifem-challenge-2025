0b3dab7b82b694b7d5221c42d0fee3186cf1cdeb
ryushen-tan <ryushen@gmail.com>
2025-01-25 18:03:04 -0500

    Update navbar.jsx

--- fe/src/components/navbar.jsx
+++ fe/src/components/navbar.jsx
@@ -8,10 +8,10 @@ const Navbar = () => {
                     <div className="bg-[#F1F9FF] w-[50vw] position-sticky mt-6 font-poppins text-white rounded-xl h-16 flex justify-left items-center">
                         <div className='rounded-full bg-white w-[3vw] h-[3vw] ml-4'></div>
                         <div className='flex gap-12 ml-12 font-medium text-[#8DCCFF] font-poppins justify-center items-center'>
-                            <hr className='bg-white w-64 h-2'/>
-                            <h1>Home</h1>
+                            <hr className='bg-white rounded-md w-64 h-2 '/>
+                            {/* <h1>Home</h1>
                             <h1>About</h1>
-                            <h1>Contact Us</h1>
+                            <h1>Contact Us</h1> */}
                         </div>
                     </div>
                 </div>
