a83f303c642a6b9ac77b775ed9ee316fb36aef32
q <167793812+0vp@users.noreply.github.com>
2025-01-25 21:43:31 -0500

    async return -> see text before lol

--- be/speech.py
+++ be/speech.py
@@ -1,9 +1,24 @@
-import os
+import os, threading
+import pyttsx3
 
 def say(text):
     # dont roast me for this LOL, it be how it be
     # os.system(f'say {text} -o speech')
-    os.system(f'say "{text}"')
+    def threaded_say():
+        os.system(f'say "{text}"')
+    
+    thread = threading.Thread(target=threaded_say)
+    thread.start()
+
+# # for windows LL
+# def say(text):
+#     def threaded_say():
+#         engine = pyttsx3.init()
+#         engine.say(text)
+#         engine.runAndWait()
+
+#     thread = threading.Thread(target=threaded_say)
+#     thread.start()
 
 if __name__ == '__main__':
     say("hello my name is bloom, how can i help you?")
